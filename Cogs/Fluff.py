5b7fa97e59b0aa116cee7af78c27a35851a2b382
SMC242 <mcalonansam2@gmail.com>
2020-08-21 09:08:59 +0100

    Made DTWMChanWorship lighter
    Invokers are now stored as strings instead of `Member`s to reduce memory cost
    
    Cooldown added to silence_chanting, get_chants, and top_cultists
    
    Now using context manager in top_cultists as it prevents infinite typing on an error
    
    get_chants now ensures that a string name exists before using it
    
    Debugging tool added to DTWMChanWorship
    
    Fixed bug when passing a period into get_chants

--- Cogs/Fluff.py
+++ Cogs/Fluff.py
@@ -8,18 +8,20 @@ from datetime import datetime
 from asyncio import sleep as async_sleep
 from Utils.memtils import get_title, NameParser
 from Utils.react_menu import ReactTable
+from json import dumps
 
 class DTWMChanWorship(commands.Cog):
     """This Cog handles chanting 'DTWM'"""
-    Chant = namedtuple("chant", ("person", "timestamp"))
+    Chant: Tuple[str, datetime] = namedtuple("chant", ("name", "timestamp"))
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.chants: List[self.Chant] = []
         self.silenced_at: datetime = None  # when the bot was last silenced
 
-    @commands.command(aliases = ["SC", "silence", "shut_up", "clear_comms"])
+    @commands.command(aliases = ["silence", "shut_up", "clear_comms"])
     @commands.has_any_role(*leader_roles)
+    @commands.cooldown(1, 300, commands.BucketType.user)  # 5 minute cooldown
     async def silence_chanting(self, ctx):
         """Clear the current chants and prevent chanting for five minutes."""
         self.chants = []
@@ -41,11 +43,11 @@ class DTWMChanWorship(commands.Cog):
                                   "Please revere DTWM-chan silently")
 
         # record the chant
-        self.chants.append(self.Chant(person = ctx.author, timestamp = datetime.now()))
+        self.chants.append(self.Chant(name = ctx.author.display_name, timestamp = datetime.now()))
 
         # create the DTWM chant and split it into chunks that can be sent
         CHARACTER_CAP = 2000
-        msg = " ".join(["DTWM"] * (len(self.chants) + 1))
+        msg = " ".join(["DTWM"] * (len(self.chants)))
         if len(msg) > CHARACTER_CAP:
             msgs = [msg[chunk_num: chunk_num + CHARACTER_CAP]
                     for chunk_num in range(0, len(msg), CHARACTER_CAP)]
@@ -61,39 +63,36 @@ class DTWMChanWorship(commands.Cog):
                         delete_after = 10)
 
     @commands.command(aliases = ["leaderboard", "TCu"])
+    @commands.cooldown(1, 20, commands.BucketType.user)
     async def top_cultists(self, ctx):
         """List the most devout worshippers of DTWM-chan."""
         # check if there has been no chants
         if not self.chants:
             return await ctx.send("The servitor bay is silent. We need more brothers to pray")
 
-        # create typing prompt without indenting as the search is already pretty deep
-        typing_indicator = await ctx.typing().__aenter__()
-        # get each unqiue person and prepare to count their chants
-        leaderboard = [[person, 0] for person in
-                                                set((chant.person for chant in self.chants))]
-        # for each message, find its author and increment their points
-        for chant in self.chants:
-            for index, row in enumerate(leaderboard):
-                if row[0] == chant.person:
-                    leaderboard[index][1] += 1
-                    break
-
-        # convert people to string and sort by points
-        string_leaderboard = sorted([(person.display_name, points) for person, points in leaderboard],
-                                    key = lambda record: record[1], reverse = True)
+        async with ctx.typing():
+            # get each unqiue person and prepare to count their chants
+            leaderboard = [[name, 0] for name in
+                                                    set((chant.name for chant in self.chants))]
+            # for each message, find its author and increment their points
+            for chant in self.chants:
+                for index, row in enumerate(leaderboard):
+                    if row[0] == chant.name:
+                        leaderboard[index][1] += 1
+                        break
     
-        ReactTable(("Name", "Number of Chants"), 
-                   string_leaderboard,
-                   self.bot,
-                   ctx,
-                   f"Our brothers have chanted {len(self.chants)} times in total. " + 
-                   "Here are DTWM-chan's most loyal worshippers:",
-                   elements_per_page = 3,
-                    )
-        await typing_indicator.__aexit__(*[None] * 3)  # pretend that no errors happened
+            ReactTable(("Name", "Number of Chants"), 
+                       sorted(leaderboard,  # sort by points
+                              key = lambda record: record[1], reverse = True),
+                       self.bot,
+                       ctx,
+                       f"Our brothers have chanted {len(self.chants)} times in total. " + 
+                       "Here are DTWM-chan's most loyal worshippers:",
+                       elements_per_page = 3,
+                        )
 
     @commands.command(aliases = ["my_chants", "GC", "chants"])
+    @commands.cooldown(1, 3, commands.BucketType.user)
     async def get_chants(self, ctx, target: Union[Member, str] = None, days: int = None):
         """Get the number of chants a person has done. Invoke without a name to get your own count.
         Defaults to their lifetime count, but you can pass a number of days (E.G today = 0)."""
@@ -101,20 +100,24 @@ class DTWMChanWorship(commands.Cog):
         if not self.chants:
             return await ctx.send("The servitor bay is silent. We need more brothers to pray")
 
-        # get the Member object of the person
-        if isinstance(target, str):
-            target_member = ctx.guild.query_members(target)
-        else:
-            target_member = target if target else ctx.author
+        # get the name of the person
+        if not isinstance(target, str):
+            target_name = target.display_name if target else ctx.author.display_name
+        else:  # check that the person exists
+            matches = await ctx.guild.query_members(target)
+            if matches:
+                target_name = matches[0].display_name
+            else:
+                return await ctx.send("That person does not exist, " + get_title(ctx.author))
 
         lifetime_check = days is None  # used for overriding the time check if it's a lifetime count
         now = datetime.now()
         count = 0
         for chant in self.chants:
-            if chant.person == target_member and (lifetime_check or \
-                (now - chant.timestamp).days <= period):  # check that it falls within the period
+            if chant.name == target_name and (lifetime_check or \
+                (now - chant.timestamp).days <= days):  # check that it falls within the period
                 count += 1
-        await ctx.send(f"{target_member.display_name} has chanted {count} times")
+        await ctx.send(f"{target_name} has chanted {count} time{'s' if count > 1 else ''}")
 
     @commands.command(aliases = ["SCC"])
     @commands.is_owner()
@@ -123,6 +126,14 @@ class DTWMChanWorship(commands.Cog):
         self.chants.extend([self.Chant(person = ctx.author,
                                        timestamp = datetime.now())] * count)
         await ctx.send(f"I have added {count} chants, Adept {ctx.author.display_name}.")
+
+    @commands.command(aliases = ["PC"])
+    @commands.is_owner()
+    async def print_chants(self, ctx):
+        """Output self.chants. Debugging tool."""
+        await ctx.send("```\n" + 
+                       dumps(self.chants, indent = 4, sort_keys = True, default = str) +
+                       "```")
 
     
 def setup(bot: commands.Bot):
