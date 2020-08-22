a18fc4bc51f7dd5739864bfb88ab7f58b0008ef9
SMC242 <56912943+SMC242@users.noreply.github.com>
2020-08-22 05:17:47 +0100

    Testing patch command

--- Utils/mestils.py
+++ Utils/mestils.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot, transforms
 from asyncio import sleep as async_sleep
 import os, re, datetime as D
 
-# test
+# test 2
 
 def create_table(cell_contents: Iterable[Iterable[Any]], file_name: str = None,
                        col_labels: List[str] = None, row_labels: List[str] = None) -> str:
