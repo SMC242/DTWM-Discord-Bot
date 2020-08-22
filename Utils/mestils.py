17a2bd2f3470870eecf176e4b8ad42304890c180
SMC242 <56912943+SMC242@users.noreply.github.com>
2020-08-22 05:06:48 +0100

    Testing patch command

--- Utils/mestils.py
+++ Utils/mestils.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot, transforms
 from asyncio import sleep as async_sleep
 import os, re, datetime as D
 
-# TODO: create a Messagable.send wrapper that chops large messages down into smaller messages
+# done
 
 def create_table(cell_contents: Iterable[Iterable[Any]], file_name: str = None,
                        col_labels: List[str] = None, row_labels: List[str] = None) -> str:
