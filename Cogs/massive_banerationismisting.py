2cf7a470c610f35d77db31a56b778237819369e9
SMC242 <smc242@users.noreply.github.com>
2021-01-08 22:48:38 +0000

    Ran into an internal server error with purge

--- Cogs/massive_banerationismisting.py
+++ Cogs/massive_banerationismisting.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from discord import Member
 from Utils.common import leader_roles
+from traceback import print_exc
 
 
 class MessageScrubber(commands.Cog):
@@ -13,14 +14,32 @@ class MessageScrubber(commands.Cog):
     @commands.command()
     async def scrub_messages(self, ctx: commands.Context, person: Member):
         """Delete all of a member's messages."""
-        progress_msg = await ctx.send("0 channels covered")
+        print(person.display_name)
+        progress_msg = await ctx.send("Channels covered:\n")
         for channel in ctx.guild.text_channels:
-            perms = channel.permissions_for(self.bot.user)
-            if not perms.manage_messages or not perms.read_message_history:
+            perms = channel.permissions_for(ctx.me)
+            if not all((perms.manage_messages, perms.read_message_history, perms.read_messages)) and not perms.administrator:
                 await ctx.send(f"{channel.name} skipped due to lack of permissions")
                 continue
             await channel.purge(check=lambda m: m.author == person)
-            await progress_msg.edit(content=f"{progress_msg.content[0] + 1} channels covered")
+            await progress_msg.edit(content=progress_msg.content + f"\n{channel.name}")
+        await ctx.send("Done")
+
+    @commands.has_any_role(*leader_roles)
+    @commands.command()
+    async def test(self, ctx: commands.Context, person: Member):
+        """Delete all of a member's messages."""
+        print(person.display_name)
+        progress_msg = await ctx.send("Channels covered:\n")
+        for channel in ctx.guild.text_channels:
+            perms = channel.permissions_for(ctx.me)
+            if not all((perms.manage_messages, perms.read_message_history, perms.read_messages)) and not perms.administrator:
+                await ctx.send(f"{channel.name} skipped due to lack of permissions")
+                continue
+            async for msg in channel.history(limit=None):
+                if msg.author == person:
+                    await msg.delete()
+            await progress_msg.edit(content=progress_msg.content + f"\n{channel.name}")
         await ctx.send("Done")
 
     @scrub_messages.error
@@ -28,7 +47,8 @@ class MessageScrubber(commands.Cog):
         if isinstance(error, commands.ConversionError):
             return ctx.send("That is not a person")
         else:
-            raise error
+            print_exc()
+            await ctx.send("Error occurred")
 
 
 def setup(bot: commands.Bot):
